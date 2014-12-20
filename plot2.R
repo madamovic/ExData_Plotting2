6b1d272a8ae0b512ff240b2c40ffea0ccd8752ca
madamovic <madamovickrivaja@gmail.com>
2014-12-20 21:33:38 +0100

    Adding R files and plots

---
+++ plot2.R
@@ -0,0 +1,9 @@
+NEI <- readRDS("data/summarySCC_PM25.rds")
+SCC <- readRDS("data/Source_Classification_Code.rds")
+BaltimoreCity <- subset(NEI, fips == "24510")
+totalPM25ByYear <- tapply(BaltimoreCity$Emissions, BaltimoreCity$year, sum)
+png("plot2.png")
+plot(names(totalPM25ByYear), totalPM25ByYear, type="l",
+     xlab="Year", ylab=expression("Total" ~ PM[2.5] ~ "Emissions (tons)"),
+     main=expression("Total Baltimore City" ~ PM[2.5] ~ "Emissions by Year"))
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,13 @@
+library(plyr)
+library(ggplot2)
+NEI <- readRDS("data/summarySCC_PM25.rds")
+SCC <- readRDS("data/Source_Classification_Code.rds")
+BaltimoreCity <- subset(NEI, fips == "24510")
+typePM25ByYear <- ddply(BaltimoreCity, .(year, type), function(x) sum(x$Emissions))
+colnames(typePM25ByYear)[3] <- "Emissions"
+png("plot3.png")
+qplot(year, Emissions, data=typePM25ByYear, color=type, geom="line") +
+  ggtitle(expression("Baltimore City" ~ PM[2.5] ~ "Emissions by Source Type and Year")) +
+  xlab("Year") +
+  ylab(expression("Total" ~ PM[2.5] ~ "Emissions (tons)"))
+dev.off()
